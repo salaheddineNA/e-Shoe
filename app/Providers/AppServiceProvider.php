ac61f1280d3df725d295b23a5f1a8a086b437e53
salaheddineNA <salahnazrhan@gmail.com>
2026-05-09 16:23:33 +0100

    Update environment configuration and service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Railway gère HTTPS automatiquement, pas besoin de forcer le schéma
     }
 }
